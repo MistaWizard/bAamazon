8366576fe85f334ee50a2c3364772a6ac2d190e6
MistaWizard <tylermckellar@gmail.com>
2019-01-29 18:32:34 -0500

    Manager donish

--- bamazonManager.js
+++ bamazonManager.js
@@ -22,3 +22,52 @@ const connection = mysql.createConnection({
   password: pwd,
   database: "bamazon"
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    startPrompt();
+});
+
+function startPrompt() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "actions";
+            message: "Welcome Michael Scott. What would you like to review?",
+            choices: ["View Products For Sale", "View Low Inventory", "Add To Inventory", "Add New Product"]
+        }
+    ]).then(function(user) {
+        if (user.actionList === "View Products For Sale") {
+            inventory();
+        } else if (user.actionList === "View Low Inventory") {
+            lowInventory();
+        } else if (user.actionList === "Add To Inventory") {
+            addInventory();
+        } else {
+            addProduct();
+        }
+    });
+};
+
+inventory = function(){
+    let query = "Select * FROM products";
+    let table = new Table ({
+		head: ["Item ID".blue, "Product Name".blue, "Catergory".blue, "Price".blue, "Quantity".blue],
+		colWidths: [10,25,25,10,14]
+	});
+	connection.query(query, function(err, res){
+		if (err) throw err;
+		for (var i = 0; i < res.length; i++){
+			table.push(
+				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+			);
+		}
+        console.log("");
+        console.log("********************************** Current bAmazon Inventory **********************************".yellow);
+        console.log("");
+        console.log(table.toString());
+        console.log("");
+		continuePrompt();
+	});
+};
